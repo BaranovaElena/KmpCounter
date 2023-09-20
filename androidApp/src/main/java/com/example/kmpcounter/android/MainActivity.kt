e3be29123e244f07fe2f9e9e3f839d7754d186c9
Elena Baranova <baranovaeaa@yandex.ru>
2023-09-20 15:32:57 +0200

    android code

--- androidApp/src/main/java/com/example/kmpcounter/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmpcounter/android/MainActivity.kt
@@ -3,24 +3,17 @@ package com.example.kmpcounter.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.kmpcounter.Greeting
+import com.example.kmpcounter.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
+                App()
             }
         }
     }
